e7b1926b5fba356fd03370f7a6fc8e10167bb4f3
Vojtech Juranek <vojtech.juranek@gmail.com>
2015-04-26 19:52:50 +0200

    HW2: Implement matrix wrapper which caches inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for creating matrix wrappper, which stores, besides matrix object 
+## itself, also inverse matrix upon inverse matrix computation and returns this 
+## cached value when requested next time.
 
-## Write a short comment describing this function
 
+## Creates matrix wrapper - list of functions for storing and getting matrix 
+## object itself and inverse matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+    
 }
 
 
-## Write a short comment describing this function
-
+## Computes matrix inverse for matrix wrapper and stores the results in 
+## the cache. If there's already inverse matrix computed in the cache, 
+## return it instread computing it again.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("returning cached inverse matrix")
+        return(inv)
+    }
+    matrix <- x$get()
+    inv <- solve(matrix, ...)
+    message("inverse matrix computed, storing result into the cache")
+    x$setInverse(inv)
+    inv
 }
+
